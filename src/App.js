9d9435dd4c98fd0369d08b58ab166d4c7dd7b0dc
Mr-Bollky <bollky.nana@gmail.com>
2022-01-21 00:46:50 +0000

    ok

--- src/App.js
+++ src/App.js
@@ -15,29 +15,6 @@ import NonAuthLayout from "./components/NonAuthLayout"
 // Import scss
 import "./assets/scss/theme.scss"
 
-// Import Firebase Configuration file
-// import { initFirebaseBackend } from "./helpers/firebase_helper"
-
-// Import fackbackend Configuration file
-import fakeBackend from "./helpers/AuthType/fakeBackend"
-
-// Activating fake backend
-fakeBackend()
-
-// Activating fake firebase
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_APIKEY,
-//   authDomain: process.env.REACT_APP_AUTHDOMAIN,
-//   databaseURL: process.env.REACT_APP_DATABASEURL,
-//   projectId: process.env.REACT_APP_PROJECTID,
-//   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-//   appId: process.env.REACT_APP_APPID,
-//   measurementId: process.env.REACT_APP_MEASUREMENTID,
-// };
-
-// init firebase backend
-// initFirebaseBackend(firebaseConfig);
 
 class App extends Component {
   constructor(props) {
--- src/pages/Produit/ModifierProduit.js
+++ src/pages/Produit/ModifierProduit.js
@@ -95,10 +95,6 @@ class ModifierProduit extends Component {
       headers: { 'Content-Type': 'application/json','Accept': 'application/json','Access-Control-Allow-Origin':'*'},
       body: JSON.stringify(data)
     };
-    const requestOptions2 = {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json','Accept': 'application/json','Access-Control-Allow-Origin':'*'},
-    };
 
     if(($("#nom").val()=="") || ($("#type_id option:selected").val()=="0") ){
       $("#initial").show()
